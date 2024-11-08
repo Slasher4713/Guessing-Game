3572c5a9313534a337c9f2223fb2e2d9b429a636
lucamuel542 <lucamuel542@SDS-L-DLFDF63>
2024-11-08 10:27:12 -0500

    Add project files.

---
+++ Guessing Game/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.numInput = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.numOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameLabel.Location = new System.Drawing.Point(301, 9);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(166, 25);
+            this.gameLabel.TabIndex = 0;
+            this.gameLabel.Text = "Guessing Game";
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Location = new System.Drawing.Point(290, 74);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(177, 13);
+            this.instructionsLabel.TabIndex = 1;
+            this.instructionsLabel.Text = "Enter a Number Between 1 and 100";
+            // 
+            // numInput
+            // 
+            this.numInput.Location = new System.Drawing.Point(332, 128);
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(100, 20);
+            this.numInput.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(341, 166);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess!";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // numOutput
+            // 
+            this.numOutput.AutoSize = true;
+            this.numOutput.Location = new System.Drawing.Point(357, 293);
+            this.numOutput.Name = "numOutput";
+            this.numOutput.Size = new System.Drawing.Size(0, 13);
+            this.numOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numOutput);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.gameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.TextBox numInput;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label numOutput;
+    }
+}
+
---
+++ Guessing Game/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_Game
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        int rando;
+        int count;
+        int distance;
+
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rando = randGen.Next(1, 101);
+            count = 0;
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            count++;
+            num = Convert.ToInt32(numInput.Text);
+            distance = rando - num;
+            int hint = Math.Abs(distance);   
+
+            if (num == rando)
+            {
+                numOutput.Text = "You Win!";
+                numOutput.Text += $"\nIt took you {count} times";
+                count = 0;
+            }
+            else if (hint <= 4)
+            {
+                numOutput.Text = "Boiling!";
+            }
+            else if (hint <= 10)
+            {
+                numOutput.Text = "Hot!";
+            }
+            else if (hint <= 10)
+            {
+                numOutput.Text = "Hot!";
+            }
+            else if (hint <= 15)
+            {
+                numOutput.Text = "Warm!";
+            }
+            else if (hint <= 25)
+            {
+                numOutput.Text = "Cool!";
+            }
+            else if (hint < 50)
+            {
+                numOutput.Text = "Cold!";
+            }
+            else if (hint >= 50)
+            {
+                    numOutput.Text = "Freezing!";
+            }
+
+            if (num > rando)
+            {
+                numOutput.Text += "\nToo High!";
+            }
+
+            if (num < rando)
+            {
+                numOutput.Text += "\nToo Low!";
+            }
+
+        }
+    }
+}
